17b8a587c2ef3af75831c58ca66747f70c32c92f
kimmihyeon12 <76936708+kimmihyeon12@users.noreply.github.com>
2022-10-04 16:12:58 +0900

    hostListener 사용하기

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import {
+  Directive,
+  Renderer2,
+  OnInit,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 //이게더 좋은 방법
 //제공하는 랜더러를 사용하는 방법
 @Directive({
@@ -12,6 +18,22 @@ export class BetterHighlightDirective implements OnInit {
       this.elRef.nativeElement,
       'background-color',
       'blue'
+    );
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'blue'
+    );
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'transparent'
     );
   }
 }
