99fa24d0c43fa48d1ecd56f685395a2e4dee7e0c
kimmihyeon12 <76936708+kimmihyeon12@users.noreply.github.com>
2022-10-04 16:26:45 +0900

    hostbing / @input binding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -4,6 +4,8 @@ import {
   OnInit,
   ElementRef,
   HostListener,
+  HostBinding,
+  Input,
 } from '@angular/core';
 //이게더 좋은 방법
 //제공하는 랜더러를 사용하는 방법
@@ -11,29 +13,35 @@ import {
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'blue';
+  @Input() highlightColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   //https://angular.io/api/core/Renderer2
   ngOnInit() {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    this.backgroundColor = this.defaultColor;
   }
 }
